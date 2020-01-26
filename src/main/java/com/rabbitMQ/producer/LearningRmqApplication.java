f1675e8f8c72652edd061b19044e1a176765faf9
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-26 02:54:10 -0500

    Update LearningRmqApplication.java

--- src/main/java/com/rabbitMQ/producer/LearningRmqApplication.java
+++ src/main/java/com/rabbitMQ/producer/LearningRmqApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
@@ -25,7 +26,8 @@ public class LearningRmqApplication implements CommandLineRunner {
 
 		rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange",
 				"topic",
-				"**************** Hello from program ****************");
+				"**************** Hello from program at "+ LocalDateTime.now().toString()
+						+"****************");
 
 		Student student = new Student(1001L, "XZY");
 		rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange","topic",student.toString());
@@ -34,10 +36,14 @@ public class LearningRmqApplication implements CommandLineRunner {
 		IntStream range2 = IntStream.rangeClosed(1, 10);
 		int[] intList = range2.parallel().toArray();
 
-		for (int x : intList) {
+		/*for (int x : intList) {
 			Thread.sleep(1000);
 			rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange","topic",Integer.toString(x));
-		}
+		}*/
 
+		rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange",
+				"topic",
+				"**************** END OF PROGRAM "+ LocalDateTime.now().toString() +
+						"****************");
 	}
 }
