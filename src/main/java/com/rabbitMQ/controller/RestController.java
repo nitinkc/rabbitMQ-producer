36f14e073314fdb6dbb774ae9b366c3ba4de1c35
Nitin Chaurasia <gs.nitin@gmail.com>
2020-07-19 10:15:14 -0400

    updates

---
+++ src/main/java/com/rabbitMQ/controller/RestController.java
@@ -0,0 +1,8 @@
+package com.rabbitMQ.controller;
+
+import org.springframework.boot.devtools.restart.RestartScope;
+
+@RestController
+public class RestController {
+
+}
--- src/main/java/com/rabbitMQ/producer/RabbitMQProducer.java
+++ src/main/java/com/rabbitMQ/producer/RabbitMQProducer.java
@@ -10,40 +10,40 @@ import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class RabbitMQConsumer implements CommandLineRunner {
+public class RabbitMQProducer implements CommandLineRunner {
 
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
 	public static void main(String[] args) {
-		SpringApplication.run(RabbitMQConsumer.class, args);
+		SpringApplication.run(RabbitMQProducer.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		//rabbitTemplate.convertAndSend("Hello World from my Program");
-		//rabbitTemplate.convertAndSend("TestExchange","testRouting","Hello from program");
+		rabbitTemplate.convertAndSend("Hello World from my Program");
+		rabbitTemplate.convertAndSend("TestExchange","testRouting","Hello from program");
 
 		rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange",
-				"topic",
-				"**************** Hello from program at "+ LocalDateTime.now().toString()
-						+"****************");
+									"topic",
+									"**************** Hello from program at "+ LocalDateTime.now().toString()+"****************");
 
 		Student student = new Student(1001L, "XZY");
 		rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange","topic",student.toString());
 
 		//Sending Parallel Stream to the Messaging Queue
-		IntStream range2 = IntStream.rangeClosed(1, 10);
+		/*
+		 * IntStream range2 = IntStream.rangeClosed(1, 100);
 		int[] intList = range2.parallel().toArray();
 
-		/*for (int x : intList) {
-			Thread.sleep(1000);
-			rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange","topic",Integer.toString(x));
-		}*/
+		
+		 * for (int x : intList) { Thread.sleep(1000);
+		 * rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange","topic",Integer.
+		 * toString(x)); }
+		 */
 
 		rabbitTemplate.convertAndSend("MyProgrammaticTopicExchange",
 				"topic",
-				"**************** END OF PROGRAM "+ LocalDateTime.now().toString() +
-						"****************");
+				"**************** END OF PROGRAM "+ LocalDateTime.now().toString() + "****************");
 	}
 }
